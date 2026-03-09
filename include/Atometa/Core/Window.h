f359eb2662e040d021d163dbefe447ee49402d76
khadr <abkhadr@hotmail.com>
2026-03-09 07:26:35 +0200

    fix: MinGW bigobj + cstdint + disable PCH for CI Windows build

--- include/Atometa/Core/Window.h
+++ include/Atometa/Core/Window.h
@@ -1,55 +1,59 @@
 #pragma once
 
-#include "Core.h"
+#include "Atometa/Core/Core.h"
+#include <cstdint>
 #include <string>
+#include <functional>
 
 struct GLFWwindow;
 
 namespace Atometa {
+
     struct WindowProperties {
         std::string Title;
-        uint32_t Width;
-        uint32_t Height;
-        bool VSync;
+        uint32_t    Width;
+        uint32_t    Height;
+        bool        VSync;
         std::string IconPath;
 
-        WindowProperties(const std::string& title = "Atometa",
-                        uint32_t width = 1600,
-                        uint32_t height = 900,
-                        bool vsync = true,
-                        const std::string& iconPath = "")
-            : Title(title), Width(width), Height(height), VSync(vsync), IconPath(iconPath) {}
+        explicit WindowProperties(const std::string& title    = "Atometa",
+                                  uint32_t           width    = 1600,
+                                  uint32_t           height   = 900,
+                                  bool               vsync    = true,
+                                  const std::string& iconPath = "")
+            : Title(title), Width(width), Height(height),
+              VSync(vsync), IconPath(iconPath) {}
     };
 
     class Window {
     public:
-        Window(const WindowProperties& props = WindowProperties());
+        explicit Window(const WindowProperties& props);
         ~Window();
 
         void OnUpdate();
-        
-        uint32_t GetWidth() const { return m_Data.Width; }
-        uint32_t GetHeight() const { return m_Data.Height; }
-        float GetAspectRatio() const { return (float)m_Data.Width / (float)m_Data.Height; }
-        
-        void SetVSync(bool enabled);
-        bool IsVSync() const { return m_Data.VSync; }
-
-        GLFWwindow* GetNativeWindow() const { return m_Window; }
         bool ShouldClose() const;
 
-        void SetWindowIcon(const std::string& iconPath);
+        uint32_t GetWidth()      const { return m_Data.Width; }
+        uint32_t GetHeight()     const { return m_Data.Height; }
+        float    GetAspectRatio()const { return (float)m_Data.Width / (float)m_Data.Height; }
+        bool     IsVSync()       const { return m_Data.VSync; }
+
+        void SetVSync(bool enabled);
+
+        GLFWwindow* GetNativeWindow() const { return m_Window; }
 
     private:
         void Init(const WindowProperties& props);
         void Shutdown();
 
-        GLFWwindow* m_Window;
+        GLFWwindow* m_Window = nullptr;
+
         struct WindowData {
             std::string Title;
-            uint32_t Width, Height;
-            bool VSync;
-        };
-        WindowData m_Data;
+            uint32_t    Width  = 1600;
+            uint32_t    Height = 900;
+            bool        VSync  = true;
+        } m_Data;
     };
-}
+
+} // namespace Atometa
